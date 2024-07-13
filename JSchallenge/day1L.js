720088409cce911fb4650d242a45e9fae15b3ddd
Suman Savarnya <125311936+sforsehlenge@users.noreply.github.com>
2024-07-13 15:57:17 +0530

    it is a day1

---
+++ JSchallenge/day1L.js
@@ -0,0 +1,36 @@
+let a= 5;
+a=2;
+// let a=3 let can be updated but not redeclare
+console.log(a)
+
+var b=3;
+b=7;
+var b=89  // var can be update and redeclare
+console.log(b);
+
+const c= 34;
+//c=45; const can neither be update nor be redeclare
+console.log(c);
+
+// var a=88;
+// console.log(a);
+
+var s
+console.log(s)
+let d;
+console.log(d);
+const e=4;
+console.log(e);
+
+console.table([a,b,c,s,d,e]);
+
+const student ={
+    n_ame:"suman",
+    mon_no:"8271980713",
+    marks:"65",
+    exam:"mid-term"
+}
+console.log(student.n_ame);
+console.table(student);
+
+
---
+++ JSchallenge/day1T.js
@@ -0,0 +1,26 @@
+var a=45;
+console.log(a);
+let b= 'suman';
+console.log(b);
+
+const c= true;
+console.log(typeof(c));
+
+let d= 45;
+let e="master";
+let f= true;
+let g;
+let h={
+    n_ame:'suman',
+    year:"3rd"
+};
+let i=[1,2,3,4,5];
+console.table([typeof(d),typeof(e),typeof(f), typeof(g),typeof(h),typeof(i)]);
+
+let r=34;
+console.log(r);
+r=45;
+console.log(r);
+const rs=34;
+rs=45;
+console.log(rs);
