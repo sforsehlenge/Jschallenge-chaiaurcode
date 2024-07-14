afa1b132c66f416e0f3b53b2d15fee8b850c17d0
Suman Savarnya <125311936+sforsehlenge@users.noreply.github.com>
2024-07-14 12:00:01 +0530

    day 2

---
+++ JSchallenge/day02T.js
@@ -0,0 +1,19 @@
+let a=5 , b=2
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+let c=4;
+c+=2;
+console.log(c);
+console.log(c-=3);
+
+let d=40;
+console.log(d>20);
+console.log(d>=40);
+console.log(d<30);
+console.log(d<=40);
+console.log('a'===a);
+console.log(a==a);
---
+++ Practice Set/PS01.js
@@ -0,0 +1,14 @@
+let a = 'suman';
+let b=1;
+console.log(a+' '+b);
+console.log(typeof(a +' '+ b));
+
+const obj = {
+    n_ame: 'suman',
+    roll: '22135130',
+    branch: 'mechanical'
+}
+obj.roll='22135131';
+obj.year='3rd';
+console.log(obj)
+
